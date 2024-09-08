ef85475f36b8d0069ed8135fc107d95848d67303
Kareem <kareemy9000@gmail.com>
2024-09-09 01:48:21 +0400

    whatkksup

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -55,112 +55,130 @@ const HomePage = () => {
     }, []);
 
     return (
-        <div className="page-content">
-            <div className="home-container">
-                <div className="full-width-banner">
-                    <section className="banner-section">
-                        <div className="banner-text-container">
-                            <h2 className="banner-title">Vintage Carousel Horses, Antiques, Paintings.</h2>
-                        </div>
-                        <div className="banner">
-                            {carouselItems.map((item, index) => (
-                                <img
-                                    src={item.image}
-                                    alt={item.name}
-                                    key={index}
-                                    className="banner-image"
-                                    style={{"--i": index}} // Assign the index as a CSS variable
-                                />
-                            ))}
-                        </div>
-                        <a href="/collection" className="banner-link">View our full collection here</a>
-                    </section>
-                </div>
+      <div className="page-content">
+        <div className="home-container">
+          <div className="full-width-banner">
+            <section className="banner-section">
+              <div className="banner-text-container">
+                <h2 className="banner-title">
+                  Vintage Carousel Horses, Antiques, Paintings.
+                </h2>
+              </div>
+              <div className="banner">
+                {carouselItems.map((item, index) => (
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    key={index}
+                    className="banner-image"
+                    style={{ "--i": index }} // Assign the index as a CSS variable
+                  />
+                ))}
+              </div>
+              <a href="/collection" className="banner-link">
+                View our full collection here
+              </a>
+            </section>
+          </div>
 
-                <section className="tagline-section">
-                    <p className="tagline">Each item on our site is one of one.</p>
-                </section>
+          <section className="tagline-section">
+            <p className="tagline">Each item on our site is one of one.</p>
+          </section>
 
-                <section className="content">
-                    <h2 className="content-heading">Timeless Carousel Horses, Rare Antiques, and Fine Paintings</h2>
-                    <div className="content-grid">
-                        <p className="content-paragraph">
-                            Welcome to Carousel World, a curated haven of meticulously restored carousel animals and fine
-                            art pieces...
-                        </p>
-                        <p className="content-paragraph">
-                            The collectible animals in our esteemed collection have been cherished by our family for
-                            generations...
-                        </p>
-                        <p className="content-paragraph">
-                            Every item tells a story of unparalleled craftsmanship, artistic vision, and the joyous
-                            spirit of generations past...
-                        </p>
-                    </div>
-                    <a href="/collection" className="cta-button">Explore Our Curated Collection</a>
-                </section>
-
-                <section className="cards-section">
-                    <div className="card about-us-card">
-                        <h2>ABOUT US</h2>
-                        <p> GET TO KNOW US</p>
-                        <div className="portraits">
-                            <div className="portrait"></div>
-                            <div className="portrait"></div>
-                            <div className="portrait"></div>
-                        </div>
-                        <button className="about-us-button">ABOUT US</button>
-                    </div>
-                    <div className="card shop-card">
-                        <h2>SHOP</h2>
-                        <h3>GO HAVE A LOOK</h3>
-                        <img src={"http://whatsintheoldbox.com/images/wallet-global.svg"} alt="Shop Icon" />
-                        <button className="shop-button">Our Collection</button>
-                    </div>
-                </section>
-
-                <section className="testimonials-section">
-                    <h2>What Our Visitors Say</h2>
-                    <Carousel
-                        showArrows={true}
-                        infiniteLoop={true}
-                        showThumbs={false}
-                        showStatus={false}
-                        swipeable={true}
-                    >
-                        {testimonials.map((testimonial, index) => (
-                            <div key={index} className="testimonial-ticket">
-                                <div className="testimonial-rating">
-                                    {[...Array(5)].map((_, i) => (
-                                        <StarIcon key={i} className={i < testimonial.rating ? 'star-filled' : 'star-empty'} />
-                                    ))}
-                                </div>
-                                <p>"{testimonial.text}"</p>
-                                <p className="testimonial-author">- {testimonial.author}</p>
-                            </div>
-                        ))}
-                    </Carousel>
-                </section>
-
-                <section className="carvers-section">
-                    <h2 className="carvers-heading">Our carousel animals are masterfully crafted by the world's most
-                        renowned artisans</h2>
-                    <div className="carvers-links">
-                        <span>Charles I.D. Loof</span>
-                        <span>Herschell Spillman Co.</span>
-                        <span>Mueller Brothers</span>
-                        <span>Dentzel</span>
-                        <span>International Masters</span>
-                    </div>
-                    <div className="carvers-images">
-                        <img src="/Aboutus/carvers2.jpg" alt="Master Carver 1" />
-                        <img src="/Aboutus/carvers3.jpg" alt="Master Carver 2" />
-                        <img src="/Aboutus/carouselrandom.jpg" alt="Master Carver 3" />
-                        <img src="/Aboutus/carvers2.jpg" alt="Master Carver 4" />
-                    </div>
-                </section>
+          <section className="content">
+            <h2 className="content-heading">
+              Timeless Carousel Horses, Rare Antiques, and Fine Paintings
+            </h2>
+            <div className="content-grid">
+              <p className="content-paragraph">
+                Welcome to Carousel World, a curated haven of meticulously
+                restored carousel animals and fine art pieces...
+              </p>
+              <p className="content-paragraph">
+                The collectible animals in our esteemed collection have been
+                cherished by our family for generations...
+              </p>
+              <p className="content-paragraph">
+                Every item tells a story of unparalleled craftsmanship, artistic
+                vision, and the joyous spirit of generations past...
+              </p>
             </div>
+            <a href="/collection" className="cta-button">
+              Explore Our Curated Collection
+            </a>
+          </section>
+
+          <section className="cards-section">
+            <div className="card about-us-card">
+              <h2>ABOUT US</h2>
+              <p> GET TO KNOW US</p>
+              <div className="portraits">
+                <img src="/Rand/1.jpg" alt="Portrait 1" className="portrait" />
+                <img src="/Rand/2.jpg" alt="Portrait 2" className="portrait" />
+                <img src="/Rand/3.jpg" alt="Portrait 3" className="portrait" />
+              </div>
+              <button className="about-us-button">ABOUT US</button>
+            </div>
+            <div className="card shop-card">
+              <h2>SHOP</h2>
+              <h3>GO HAVE A LOOK</h3>
+              <img
+                src={"http://whatsintheoldbox.com/images/wallet-global.svg"}
+                alt="Shop Icon"
+              />
+              <button className="shop-button">Our Collection</button>
+            </div>
+          </section>
+
+          <section className="testimonials-section">
+            <h2>What Our Visitors Say</h2>
+            <Carousel
+              showArrows={true}
+              infiniteLoop={true}
+              showThumbs={false}
+              showStatus={false}
+              swipeable={true}
+            >
+              {testimonials.map((testimonial, index) => (
+                <div key={index} className="testimonial-ticket">
+                  <div className="testimonial-rating">
+                    {[...Array(5)].map((_, i) => (
+                      <StarIcon
+                        key={i}
+                        className={
+                          i < testimonial.rating ? "star-filled" : "star-empty"
+                        }
+                      />
+                    ))}
+                  </div>
+                  <p>"{testimonial.text}"</p>
+                  <p className="testimonial-author">- {testimonial.author}</p>
+                </div>
+              ))}
+            </Carousel>
+          </section>
+
+          <section className="carvers-section">
+            <h2 className="carvers-heading">
+              Our carousel animals are masterfully crafted by the world's most
+              renowned artisans
+            </h2>
+            <div className="carvers-links">
+              <span>Charles I.D. Loof</span>
+              <span>Herschell Spillman Co.</span>
+              <span>Mueller Brothers</span>
+              <span>Dentzel</span>
+              <span>International Masters</span>
+            </div>
+            <div className="carvers-images">
+              <img src="/Aboutus/carvers2.jpg" alt="Master Carver 1" />
+              <img src="/Aboutus/carvers3.jpg" alt="Master Carver 2" />
+              <img src="/Aboutus/carouselrandom.jpg" alt="Master Carver 3" />
+              <img src="/Aboutus/carvers2.jpg" alt="Master Carver 4" />
+            </div>
+          </section>
         </div>
+      </div>
     );
 };
 
